06119e5135ccae0569da07dcc929aee9d99ac72b
argolinh <sosconnect39@gmail.com>
2024-05-25 13:54:29 -0300

    Delete js directory

--- js/touchscroll.js
+++
@@ -1,8 +0,0 @@
-//touchscroll.js
-//função para deslizar na tela sensivel ao toque
-document.addEventListener('touchmove', (event) => {
-    event.preventDefault(); //previne o comportamento padrao de rolagem
-
-    var touch = event.touches[0]; //obtem o primeiro toque
-    window.scrollBy(0, -touch.clientY); //deslisa a page com base no movimento do toque 
-}, false);
